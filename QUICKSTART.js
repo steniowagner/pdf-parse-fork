3a4eb25d5bb974d87e7e7dc5976c05c6cdb8ddf2
Stenio Wagner <stenio.wagner1@gmail.com>
2023-08-21 16:31:31 -0300

    changing folder strcuture

--- QUICKSTART.js
+++ QUICKSTART.js
@@ -1,14 +1,11 @@
-const PDF = require('./');
-const fs = require('fs');
+const PDF = require("./");
+const fs = require("fs");
 
-let PDF_FILE = './test/data/01-valid.pdf';
-let dataBuffer = fs.readFileSync('./test/data/01-valid.pdf');
-PDF(dataBuffer).then(function(data) {
-    fs.writeFileSync(`${PDF_FILE}.txt`, data.text, {
-        encoding: 'utf8',
-        flag: 'w'
-    });
-    debugger;
-}).catch(function(err) {
-    debugger;
-});
+let dataBuffer = fs.readFileSync("./test/data/test3.pdf");
+PDF(dataBuffer)
+	.then(function (data) {
+		console.log(data.text);
+	})
+	.catch(function (err) {
+		console.log("err: ", err);
+	});
